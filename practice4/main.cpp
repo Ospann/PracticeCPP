34da9338eb211e8c52ce2e35c40d8eca3bede856
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2023-10-20 11:47:37 +0600

    Update main.cpp

--- practice4/main.cpp
+++ practice4/main.cpp
@@ -3,23 +3,45 @@ using namespace std;
 
 class Complex
 {
+private:
+    double real;
+    double imag;
+
 public:
-    // Конструктор класса
+    /*
+     * Конструктор класса
+     * принимает два значения
+     * @realPart действительную часть
+     * @imagPart мнимая часть
+     */
     Complex(double realPart, double imagPart) : real(realPart), imag(imagPart) {}
 
-    // Метод для сложения комплексных чисел
+    /*
+     * Метод для сложения комплексных чисел
+     * Принимает на другое комплексное число помечается как @other
+     * Возвращаю Complex где сложил нынешние данные и @other
+     */
     Complex Add(const Complex &other) const
     {
         return Complex(real + other.real, imag + other.imag);
     }
 
-    // Метод для вычитания комплексных чисел
+    /*
+     * Метод для вычитания комплексных чисел
+     * @other принимает другое комплексное число
+     * @return Complex где сминусовал данные у нынешнего
+     */
     Complex Sub(const Complex &other) const
     {
         return Complex(real - other.real, imag - other.imag);
     }
 
-    // Метод для умножения комплексных чисел
+    /*
+     * Метод для умножения комплексных чисел
+     * @other принимает другое комплексное число
+     * Умножаю нынешнее действительное с действительным @other
+     * Формула калькуляции (a,b)x(c,d) =(ac–bd, ad+bc);
+     */
     Complex Mul(const Complex &other) const
     {
         double newReal = (real * other.real) - (imag * other.imag);
@@ -27,7 +49,11 @@ public:
         return Complex(newReal, newImag);
     }
 
-    // Метод для деления комплексных чисел
+    /*
+     * Метод для деления комплексных чисел
+     * @other принимает другое комплексное число
+     * формула калькуляции (a, b)/(c, d) = (ac + bd, bc – ad) / (c2 + d2).
+     */
     Complex Div(const Complex &other) const
     {
         double divisor = (other.real * other.real) + (other.imag * other.imag);
@@ -41,10 +67,6 @@ public:
     {
         cout << "(" << real << ", " << imag << ")" << endl;
     }
-
-private:
-    double real;
-    double imag;
 };
 
 int main()
